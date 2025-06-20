af2e3f88322f51f8fba1f163af8a71d3677a44d8
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-20 10:17:18 +0600

    Cretare book interface

---
+++ src/app/interfaces/book.interface.ts
@@ -0,0 +1,21 @@
+export type Genre =
+  | "FICTION"
+  | "NON_FICTION"
+  | "SCIENCE"
+  | "HISTORY"
+  | "BIOGRAPHY"
+  | "FANTASY";
+
+export interface IBOOK {
+  title: string;
+  author: string;
+  genre: Genre;
+  isbn: string;
+  description?: string;
+  copies: number;
+  available?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+
