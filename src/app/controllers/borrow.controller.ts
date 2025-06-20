d5974dd4fa251126e9aa22079d4924a492dc87a2
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-20 20:48:26 +0600

    Implemet for find borrowed book summary

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -51,4 +51,47 @@ borrowRoutes.post("/", async (req: Request, res: Response) => {
 
 // Book summary
 
+borrowRoutes.get("/", async(req: Request, res: Response) => {
+    try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: '$book',
+          totalQuantity: { $sum: '$quantity' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'books',
+          localField: '_id',
+          foreignField: '_id',
+          as: 'bookDetails',
+        },
+      },
+      {
+        $unwind: '$bookDetails',
+      },
+      {
+        $project: {
+          book: {
+            title: '$bookDetails.title',
+            isbn: '$bookDetails.isbn',
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
 
+    res.status(200).json({
+      success: true,
+      message: 'Borrowed books summary retrieved successfully',
+      data: summary,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: 'Failed to retrieve summary',
+      error: error.message,
+    });
+  }
+})
