48bd437bcb336e0228aeac807d22bfd3ed3cf5f1
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-20 20:35:03 +0600

    Create schema and model for borrow

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import { bookRoutes } from "./app/controllers/book.controller";
+import { borrowRoutes } from "./app/controllers/borrow.controller";
 
 
 const app: Application = express();
 app.use(express.json());
 
+// Books universal route
 app.use("/api/books", bookRoutes)
+
+// Borrow universal route
+app.use("/api/borrow", borrowRoutes)
 
 
 app.get("/", (req: Request, res: Response) => {
---
+++ src/app/controllers/borrow.controller.ts
@@ -0,0 +1,15 @@
+
+import express, { Request, Response, Router } from "express";
+
+
+export const borrowRoutes: Router = express.Router();
+
+
+borrowRoutes.get("/", async(req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
